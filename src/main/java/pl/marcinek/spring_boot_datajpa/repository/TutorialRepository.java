8847a5c4d33fa95d4f8aa938f9ce6df2751c7465
Kacper <witcher9387@gmail.com>
2020-09-07 11:42:55 +0200

    Initial commit

---
+++ src/main/java/pl/marcinek/spring_boot_datajpa/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package pl.marcinek.spring_boot_datajpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.marcinek.spring_boot_datajpa.model.Tutorial;
+
+import java.util.List;
+
+@Repository
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+}
---
+++ src/test/java/pl/marcinek/spring_boot_datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.marcinek.spring_boot_datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
